156f9cdbd6b1394de7af35a29faa89e451f94b48
PugzAreCute <me@pugzarecute.com>
2024-05-16 14:40:23 +0530

    fix: drop unused variable

--- src/routes/wiki/+page.server.ts
+++ src/routes/wiki/+page.server.ts
@@ -1,4 +1,4 @@
-export async function load({ params}) {
+export async function load() {
 	const wiki_doc = `https://raw.githubusercontent.com/opendroid-project/docs/dev-webrender/wiki.md`;
 
 	const markdownFetch = await fetch(wiki_doc);
