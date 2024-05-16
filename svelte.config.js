94882c0cb20ede96af216100dc09f9c9394aec68
PugzAreCute <me@pugzarecute.com>
2024-05-16 15:55:35 +0530

    fix: add vite preprocess
    * TS needs it.

--- svelte.config.js
+++ svelte.config.js
@@ -1,4 +1,5 @@
 import adapter from '@sveltejs/adapter-cloudflare';
+import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 
 export default {
 	kit: {
@@ -9,5 +10,6 @@ export default {
 				exclude: ['<all>']
 			}
 		})
-	}
+	},
+	preprocess: vitePreprocess()
 };
