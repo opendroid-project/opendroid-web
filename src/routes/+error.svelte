f9e96400588640c9c6db4375f61d8f28d726d4ec
PugzAreCute <me@pugzarecute.com>
2024-05-16 22:29:49 +0530

    feat: error page!

---
+++ src/routes/+error.svelte
@@ -0,0 +1,23 @@
+<script lang="ts">
+	import { page } from '$app/stores';
+
+	const errorMessage = $page.error ? $page.error.message : "Unkniown"
+</script>
+
+<style>
+    #container {
+        display: flex;
+        flex-direction: column;
+				align-items: center;
+				vertical-align: center;
+		}
+
+		img {
+				width: 60%;
+		}
+</style>
+
+<div id="container">
+	<h4>{$page.status}: {errorMessage}</h4>
+	<img src="https://http.dog/{$page.status}.jpg" alt="HTTP ERROR {$page.status}">
+</div>
