ad0de0b260de8ca875fea0961280d31b179bf6fe
PugzAreCute <me@pugzarecute.com>
2024-05-16 14:36:53 +0530

    feat: add wiki homepage

---
+++ src/routes/wiki/+page.svelte
@@ -0,0 +1,28 @@
+<script lang="ts">
+	import SvelteMarkdown from 'svelte-markdown';
+
+	import type { PageData } from "./$types";
+
+	export let data: PageData;
+
+	import { copyText } from 'svelte-copy';
+	import { browser } from '$app/environment';
+
+	if (browser) {
+		document.addEventListener("click", copyCodeBlock);
+	}
+	async function copyCodeBlock(event: Event) {
+		console.log(event.target);
+		if ( event.target != null) copyText(event.target.innerText);
+	}
+</script>
+
+<style>
+	#markdown_container{
+			padding: 10px;
+	}
+</style>
+
+<div id="markdown_container">
+<SvelteMarkdown source={data.markdown} />
+</div>
