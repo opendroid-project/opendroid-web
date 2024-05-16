365d9c42225479d5a02ab512c8521ce5c55dc302
PugzAreCute <me@pugzarecute.com>
2024-05-16 16:40:29 +0530

    fix: change branch to master

--- src/routes/wiki/[...path]/+page.server.ts
+++ src/routes/wiki/[...path]/+page.server.ts
@@ -1,5 +1,5 @@
 export async function load({ params}) {
-	const wiki_doc = `https://raw.githubusercontent.com/opendroid-project/docs/dev-webrender/wiki/${params.path}.md`;
+	const wiki_doc = `https://raw.githubusercontent.com/opendroid-project/docs/master/wiki/${params.path}.md`;
 
 	const markdownFetch = await fetch(wiki_doc);
 
