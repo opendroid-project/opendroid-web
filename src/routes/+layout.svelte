c1f7f694fa52a7a759cc37d9812a5557acbdcdd1
PugzAreCute <me@pugzarecute.com>
2024-05-16 14:59:14 +0530

    feat: footers

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -29,7 +29,23 @@
 				transform: scale(1.1);
 				transition-duration: var(--transition-duration);
 		}
+		footer {
+				margin-top: auto;
+				text-align: center;
+				background-color: #222;
+		}
+		footer *{
+				background-color: inherit;
+		}
 
 </style>
 
 <slot></slot>
+
+<footer>
+	<p>
+		Copyright © OpenDroid Contributors 2024 • All wiki content licensed under <a href="https://creativecommons.org/licenses/by-sa/4.0/">Creative Commons Attribution-ShareAlike</a> unless otherwise noted.
+		<br>
+		This website is licensed under <a href="https://www.gnu.org/licenses/old-licenses/lgpl-2.0.txt">GNU LGPLv2</a> • Source code available at <a href="https://github.com/opendroid-project/opendroid-web">https://github.com/opendroid-project/opendroid-web</a>
+	</p>
+</footer>
