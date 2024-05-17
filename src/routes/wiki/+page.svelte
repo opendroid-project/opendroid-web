6e2bb147b2d1b9499f9206d38c7dd40a532857b7
PugzAreCute <me@pugzarecute.com>
2024-05-17 10:38:35 +0530

    fix: sync homepage with subpages

--- src/routes/wiki/+page.svelte
+++ src/routes/wiki/+page.svelte
@@ -7,22 +7,45 @@
 
 	import { copyText } from 'svelte-copy';
 	import { browser } from '$app/environment';
+	import { toast } from '@zerodevx/svelte-toast'
+	import { onMount } from 'svelte';
+	import AnchorJS from 'anchor-js';
 
 	if (browser) {
 		document.addEventListener("click", copyCodeBlock);
 	}
 	async function copyCodeBlock(event: Event) {
-		console.log(event.target);
-		if ( event.target != null) copyText(event.target.innerText);
+		if (event.target.nodeName == "CODE"){
+			copyText(event.target.innerText);
+			toast.push("Copied!")
+		}
 	}
+
+	onMount(() => {
+		const anchors = new AnchorJS();
+		anchors.add();
+		anchors.add('h1');
+	})
 </script>
 
 <style>
 	#markdown_container{
 			padding: 10px;
 	}
+	@media only screen and (min-width: 910px){
+      #readability_container{
+          display: flex;
+          flex-direction: row;
+          justify-content: center;
+      }
+			#markdown_container{
+          width: 60vw;
+      }
+	}
 </style>
 
+<div id="readability_container">
 <div id="markdown_container">
 <SvelteMarkdown source={data.markdown} />
+</div>
 </div>
