df0445b58c5564b19e75f27bb65da4fc53647cb3
PugzAreCute <me@pugzarecute.com>
2024-05-18 11:42:31 +0530

    feat: select hamburger/desktop navigation bar automatically

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,17 +1,22 @@
 <script lang="ts">
 	import '../app.css';
 	import { SvelteToast } from '@zerodevx/svelte-toast';
+	import HamburgerNavBar from '$lib/HamburgerNavBar.svelte';
+	import DesktopNavigation from '$lib/DesktopNavigation.svelte';
+	import { browser } from '$app/environment';
+	let minWidth = false;
+	if(browser){
+		minWidth = window.matchMedia("(min-width: 500px)").matches;
+	}
 </script>
 
 <SvelteToast />
 
-<nav>
-	<a href="/contact">Contact</a>
-	<a href="/wiki">Wiki</a>
-	<a href="/">Home</a>
-	<div id="nostyle"></div>
-	<a href="/" id="odroidlogo">OpenDroid</a>
-</nav>
+{#if minWidth}
+<DesktopNavigation />
+	{:else}
+	<HamburgerNavBar />
+{/if}
 
 <slot></slot>
 
@@ -30,33 +35,6 @@
 </footer>
 
 <style>
-	nav {
-		border-bottom: 2px solid var(--text-color);
-		padding: 10px;
-		display: flex;
-		flex-direction: row-reverse;
-	}
-	#odroidlogo {
-		float: left;
-		border: none;
-		font-size: 1em;
-	}
-	#nostyle {
-		flex-grow: 100;
-		border: none;
-	}
-	nav a {
-		padding: 5px;
-		border-radius: var(--border-radius);
-		margin: 3px;
-		text-decoration: none;
-		transition-duration: var(--transition-duration);
-		border-bottom: 3px solid transparent;
-	}
-	nav a:hover {
-		border-bottom: 3px solid darkgoldenrod;
-		transition-duration: var(--transition-duration);
-	}
 	footer {
 		margin-top: auto;
 		text-align: center;
