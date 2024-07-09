eadd911df8d41ff2dee253b2eae75845f5e981b4
PugzAreCute <me@pugzarecute.com>
2024-07-09 19:31:39 +0530

    chore: migrate to proper cdn

--- src/routes/+page.server.ts
+++ src/routes/+page.server.ts
@@ -1,5 +1,5 @@
 export async function load({ params }) {
-	const contributors_json = `https://raw.githubusercontent.com/opendroid-project/docs/master/contributors.json`;
+	const contributors_json = `https://opendroid-docs-cdn.pugzarecute.com/contributors.json`;
 
 	const contributorsFetch = await fetch(contributors_json);
 
--- src/routes/wiki/+page.server.ts
+++ src/routes/wiki/+page.server.ts
@@ -1,5 +1,5 @@
 export async function load() {
-	const wiki_doc = `https://raw.githubusercontent.com/opendroid-project/docs/master/wiki.md`;
+	const wiki_doc = `https://opendroid-docs-cdn.pugzarecute.com/wiki.md`;
 
 	const markdownFetch = await fetch(wiki_doc);
 
