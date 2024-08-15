6aebba8098cf57fd146c3932ea9bb774afe5e61f
PugzAreCute <me@pugzarecute.com>
2024-08-15 06:36:37 +0530

    feat: render images (better)

---
+++ src/lib/ImageRenderer.svelte
@@ -0,0 +1,16 @@
+<script>
+	export let href = ''
+	export let title = undefined
+	export let text = ''
+</script>
+
+<style>
+	img{
+			display: block;
+			max-width: 100%;
+	}
+</style>
+
+<a href="{href}">
+<img src={href} {title} alt={text}>
+</a>
--- src/lib/WebRenderComponent.svelte
+++ src/lib/WebRenderComponent.svelte
@@ -8,6 +8,7 @@
 	import Toc from 'svelte-toc';
 	import CodeRenderer from '$lib/CodeRenderer.svelte';
 	import CodespanRenderer from '$lib/CodespanRenderer.svelte';
+	import ImageRenderer from '$lib/ImageRenderer.svelte';
 
 	onMount(() => {
 		const anchors = new AnchorJS();
@@ -18,7 +19,7 @@
 
 <div id="readability_container">
 	<div id="markdown_container">
-		<SvelteMarkdown source={markdown} renderers={{ code: CodeRenderer, codespan: CodespanRenderer}} />
+		<SvelteMarkdown source={markdown} renderers={{ code: CodeRenderer, codespan: CodespanRenderer, image: ImageRenderer}} />
 	</div>
 	<Toc --toc-mobile-bg="var(--lighter-color)" headingSelector=":is(h1,h2, h3, h4,h5,h6):not(.toc-exclude)"/>
 </div>
