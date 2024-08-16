ddc690f64d7de9dba81f8d0bf0fc6e97c6a16e1e
PugzAreCute <me@pugzarecute.com>
2024-08-16 17:57:17 +0530

    fixup! throw errors if markdown fetching fails

--- src/routes/wiki/+page.server.ts
+++ src/routes/wiki/+page.server.ts
@@ -1,7 +1,13 @@
+import { error } from '@sveltejs/kit';
+
 export async function load() {
 	const wiki_doc = `https://opendroid-docs-cdn.pugzarecute.com/wiki.md`;
 
 	const markdownFetch = await fetch(wiki_doc);
+
+	if(markdownFetch.status != 200) {
+		throw error(markdownFetch.status, markdownFetch.statusText);
+	}
 
 	const markdown = await markdownFetch.text();
 
--- src/routes/wiki/[...path]/+page.server.ts
+++ src/routes/wiki/[...path]/+page.server.ts
@@ -1,7 +1,13 @@
+import { error } from '@sveltejs/kit';
+
 export async function load({ params }) {
 	const wiki_doc = `https://opendroid-docs-cdn.pugzarecute.com/wiki/${params.path}.md`;
 
 	const markdownFetch = await fetch(wiki_doc);
+
+	if(markdownFetch.status != 200) {
+		throw error(markdownFetch.status, markdownFetch.statusText);
+	}
 
 	const markdown = await markdownFetch.text();
 
